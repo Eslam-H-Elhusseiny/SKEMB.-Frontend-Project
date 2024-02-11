fa81b55cb5d253cc2e350e3cbd4c53199741d159
Eslam Elhusseiny <eslam.hamed.elhusseiny@gmail.com>
2024-02-11 21:05:18 +0200

    added image slider and swiper library files + minor typography fixes

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// *NOTE -  Swiper Library
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
