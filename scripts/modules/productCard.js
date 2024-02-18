445f2d1f7a20c99c39e8eedd9f1ece0f321679bf
Eslam Elhusseiny <eslam.hamed.elhusseiny@gmail.com>
2024-02-18 14:35:43 +0200

    added categories and hot-offers pages

--- scripts/modules/productCard.js
+++ scripts/modules/productCard.js
@@ -15,27 +15,22 @@ const productCard = (container, productsArray, page, imgArr = "") => {
         <!-- Card -->
         <div class="card px-2 py-2 h-100">
             <div class="card-product position-relative justify-end d-flex flex-col align-center">
-                <a href="details.html?id=${product.id}"> <img src=${src} alt="${
-      product.title
-    }"/></a>
-                <button --data-pId=${
-                  product.id
-                } class="btn addToCart w-85 cursor-pointer d-inline-block">Add To Cart</button>
-                <button class="btn addToCartLoader w-85 cursor-pointer d-none"><i class="fa-solid fa-spinner fa-spin"></i></button>
+                <img src=${src} alt="${product.title}"/>
+                <a href="#" class="btn cart-btn w-85">Add To Cart</a>
                 <a href="#" class="fav position-absolute py-1 px-2"><i class="fa-regular fa-heart"></i></a>
                 <a href="#" class="fav position-absolute py-1 px-2 fav-solid"><i class="fa-solid fa-heart"></i></a>
             </div>
             <div class="card-text px-1 d-flex flex-col">
-                <a href="categories.html" class="product-cat mt-4">${
-                  product.category
-                }</a>
-                <a href="details.html?id=${product.id}">
-                  <h3 class="product-h mb-1">${product.title.slice(0, 27)}</h3>
-                </a>
-                <p class="product-p mb-3">${product.description.slice(
-                  0,
-                  125
-                )}</p>
+                <a href="${product.category}" class="product-cat mt-4">${
+      product.category
+    }</a>
+                <a href="details.html?id=${
+                  product.id
+                }"><h3 class="product-h mb-1">${product.title.slice(
+      0,
+      25
+    )}</h3></a>
+                <p class="product-p">${product.description}</p>
                 <div class="price-colors my-1 align-center justify-between d-flex" >
                     <h4 class="price-tag">
                         <span class="currency">EGP</span>
@@ -58,5 +53,4 @@ const productCard = (container, productsArray, page, imgArr = "") => {
     });
   });
 };
-
 export { productCard };
