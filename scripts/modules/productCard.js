d7adb430a3bd60f53a09192a769bcb55f244b1b4
Mohamed Hafez <mhafez296@gmail.com>
2024-02-18 05:10:33 +0200

    create add to cart actions , loading & respone (success add or faild ) , get loged user cart , deny add to cart from notLogged

--- scripts/modules/productCard.js
+++ scripts/modules/productCard.js
@@ -1,3 +1,7 @@
+import { addToCart } from "./addtocart.js";
+import LoginModal from "./loginModal.js";
+
+
 const productCard = (container, productsArray, page, imgArr = "") => {
   let i = 0;
   productsArray.forEach((product) => {
@@ -13,22 +17,14 @@ const productCard = (container, productsArray, page, imgArr = "") => {
         <div class="card px-2 py-2 h-100">
             <div class="card-product position-relative justify-end d-flex flex-col align-center">
                 <img src=${src} alt="${product.title}"/>
-                <a href="#" class="btn cart-btn w-75" onclick="addToCart(${
-                  product.id
-                })">Add To Cart</a>
+                <button --data-pId=${product.id} class="btn addToCart w-75 cursor-pointer d-inline-block">Add To Cart</button>
+                <button class="btn addToCartLoader w-75 cursor-pointer d-none"><i class="fa-solid fa-spinner fa-spin"></i></button>
                 <a href="#" class="fav position-absolute py-1 px-2"><i class="fa-regular fa-heart"></i></a>
                 <a href="#" class="fav position-absolute py-1 px-2 fav-solid"><i class="fa-solid fa-heart"></i></a>
             </div>
             <div class="card-text px-1 d-flex flex-col">
-                <a href="${product.category}" class="product-cat mt-4">${
-      product.category
-    }</a>
-                <a href="cart.html?id=${
-                  product.id
-                }"><h3 class="product-h mb-1">${product.title.slice(
-      0,
-      25
-    )}</h3></a>
+                <a href="${product.category}" class="product-cat mt-4">${ product.category }</a>
+                <a href="cart.html?id=${ product.id }"><h3 class="product-h mb-1">${product.title.slice( 0, 25 )}</h3></a>
                 <p class="product-p">${product.description}</p>
                 <div class="price-colors my-1 align-center justify-between d-flex" >
                     <h4 class="price-tag">
@@ -45,6 +41,11 @@ const productCard = (container, productsArray, page, imgArr = "") => {
         </div>
     `;
     container.appendChild(card);
+    let addToCartBtn = card.querySelector(".addToCart")
+    let addToCartLoader = card.querySelector(".addToCartLoader")
+    addToCartBtn.addEventListener('click', (e) => {
+      addToCart(product.id , addToCartBtn , addToCartLoader);
+    });
   });
 };
 
